c9eeaadd5ba1a4ac34949b2d146a2678a551a3e9
MylesTrump <>
2021-05-07 14:12:33 +0000

    Updated the blank spaces to not have whitespace

--- area_of_hexagon.py
+++ area_of_hexagon.py
@@ -8,20 +8,19 @@
 
 def main():
     # this function calculates the area of a hexagon
-    
+
     # input
     face_length = int(input("Enter the face length of the hexagon (mm): "))
-    
+
     # process
     area_of_hexagon = (((3 ** 0.5) * 3) / 2) * face_length ** 2
-    
+
     # output
     print("")
     print("Area is {0}mm²".format(area_of_hexagon))
     print("")
     print("done")
-    
-    
+
+
 if __name__ == "__main__":
     main()
-    
